f1ae2f28daf096327207bce42544de7163e9a54e
Mo <rahim.muhammad@pm.me>
2021-01-15 22:54:07 +0000

    added load balancer

--- main.tf
+++ main.tf
@@ -1,6 +1,44 @@
 provider "aws" {
-
 	region = "us-east-1"
+}
+
+resource "aws_elb" "example" {
+	name = "terraform-asg-example"
+	availability_zones = "${data.aws_availability_zones.all.names}"
+	security_groups = ["${aws_security_group.elb.id}"]
+
+	listener {
+		lb_port = 80
+		lb_protocol = "http"
+		instance_port = "${var.server_port}"
+		instance_protocol = "http"
+	}
+
+	health_check {
+		healthy_threshold = 2
+		unhealthy_threshold = 2
+		timeout = 3
+		interval = 30
+		target = "HTTP:${var.server_port}/"
+	}
+}
+
+resource "aws_security_group" "elb" {
+	name = "terraform-example-elb"
+
+	ingress {
+		from_port = 80
+		to_port = 80
+		protocol = "tcp"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+	egress {
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
 }
 
 resource "aws_instance" "example" {
@@ -53,6 +91,9 @@ resource "aws_autoscaling_group" "example" {
 	launch_configuration = "${aws_launch_configuration.example.id}"
 	availability_zones = "${data.aws_availability_zones.all.names}"
 
+	load_balancers = ["${aws_elb.example.name}"]
+	health_check_type = "ELB"
+
 	min_size = 2
 	max_size = 10
 
@@ -70,7 +111,7 @@ variable "server_port" {
 }
 
 output "public_ip" {
-	value = "${aws_instance.example.public_ip}"
+	value = "${aws_elb.example.dns_name}"
 }
 
 data "aws_availability_zones" "all" {}
